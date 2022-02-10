f55baa35067421e5678978d6af5ef861ea4d4b82
DuckBoy102 <avieln7@gmail.com>
2022-02-10 16:28:47 +0200

    enter the children number

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x;
+            int y;
+            int z;
+            Console.WriteLine("enter the children number");
+            x = int.Parse(Console.ReadLine());
+            Console.WriteLine("enter the chairs number");
+            y = int.Parse(Console.ReadLine());
+            z = (x - y);
+            if (z >= 0)
+                Console.WriteLine("we need {0} more chairs", z);
+            else
+                Console.WriteLine("ok");
+        }
+    }
+}
